2b883cd443ce2c2ad2cefd77b2ba3b4e94ea8a79
dohyeonkim2526 <veronica.kdh@gmail.com>
2024-01-04 14:57:29 +0900

    connect shell test

---
+++ plugins/select_fruit.sh
@@ -0,0 +1,14 @@
+echo "Please select fruits between (APPLE or ORANGE or GRAPE)."
+echo "You need to enter in English capital letters."
+
+FRUIT = $1 # 첫번째 입력인자
+
+if [$FRUIT == APPLE]; then
+    echo "You selected Apple!"
+elif [$FRUIT == ORANGE]; then
+    echo "You selected Orange!"
+elif [$FRUIT == GRAPE]; then
+    echo "You selected Grape!"
+else
+    echo "You selected other Fruit!"
+fi
