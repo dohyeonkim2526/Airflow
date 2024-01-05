be02f3f44279f0040374807e281ee432cf8de2b3
dohyeonkim2526 <veronica.kdh@gmail.com>
2024-01-05 14:16:31 +0900

    modify dags_bash_with_macro

--- dags/dags_bash_with_macro_eg1.py
+++ dags/dags_bash_with_macro_eg1.py
@@ -14,7 +14,7 @@ with DAG(
     # END_DATE: 1일 전으로 설정
     bash_task_1=BashOperator(
         task_id="bash_task_1",
-        env={'START_DATE':'{{ date_interval_start.in_timezone("Asia/Seoul") | ds }}',
+        env={'START_DATE':'{{ data_interval_start.in_timezone("Asia/Seoul") | ds }}',
              'END_DATE':'{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=1)) | ds}}'
             },
         bash_command='echo "START_DATE: $START_DATE" && echo "END_DATE: $END_DATE"'
