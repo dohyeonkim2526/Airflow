344f3adba0111bc323aa99c188ae85f3885256a1
dohyeonkim2526 <veronica.kdh@gmail.com>
2024-01-05 11:09:30 +0900

    dags templates upload

---
+++ dags/dags_python_show_templates.py
@@ -0,0 +1,19 @@
+from airflow import DAG
+import pendulum
+import datetime
+from airflow.decorators import task
+
+with DAG(
+    dag_id="dags_python_show_templates",
+    schedule="30 9 * * *",
+    start_date=pendulum.datetime(2024, 1, 1, tz="Asia/Seoul"),
+    catchup=True    # 24년 1/1 ~ 오늘까지 스케줄 모두 실행
+) as dag:
+    
+    @task(task_id='python_task')
+    def show_templates(**kwargs):
+        from pprint import pprint
+        pprint(kwargs)
+
+    show_templates()
+    
