d58bc131696256bf64c0245b568fdb953a3335c7
dohyeonkim2526 <veronica.kdh@gmail.com>
2024-01-05 10:25:47 +0900

    modify dags_bash_read_template.py

--- dags/dags_bash_read_template.py
+++ dags/dags_bash_read_template.py
@@ -12,14 +12,14 @@ with DAG(
     
     bash_t1=BashOperator(
         task_id="bash_t1",
-        bash_command='echo "data_interval_end: {{ data_interval_end }}"'        
+        bash_command='echo "data_interval_end: {{ data_interval_end }} "'        
     )
 
     bash_t2=BashOperator(
         task_id="bash_t2",
         env={
-            'START_DATE':'{{ data_interval_start | ds }}',
-            'END_DATE':'{{ data_interval_end | ds }}'
+            'START_DATE':'{{data_interval_start | ds }}',
+            'END_DATE':'{{data_interval_end | ds }}'
         },
         bash_command='echo $START_DATE && echo $END_DATE' # A && B : A가 성공하면 B도 실행시킨다.
     )
