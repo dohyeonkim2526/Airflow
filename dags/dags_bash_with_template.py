5b67d77e3f5dfb7659b7f27f64bf59f21314220d
dohyeonkim2526 <veronica.kdh@gmail.com>
2024-01-05 10:30:34 +0900

    upload dags_bash dag

--- dags/dags_bash_with_template.py
+++ dags/dags_bash_with_template.py
@@ -4,24 +4,24 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 
 with DAG(
-    dag_id="dags_bash_read_template",
+    dag_id="dags_bash_with_template",
     schedule="10 0 * * *",
     start_date=pendulum.datetime(2024, 1, 5, tz="Asia/Seoul"),
     catchup=False
 ) as dag:
     
-    bash_t1=BashOperator(
-        task_id="bash_t1",
-        bash_command='echo "data_interval_end: {{ data_interval_end }} "'        
+    bash_t1 = BashOperator(
+        task_id='bash_t1',
+        bash_command='echo "data_interval_end: {{ data_interval_end }}  "'
     )
 
-    bash_t2=BashOperator(
-        task_id="bash_t2",
+    bash_t2 = BashOperator(
+        task_id='bash_t2',
         env={
             'START_DATE':'{{data_interval_start | ds }}',
             'END_DATE':'{{data_interval_end | ds }}'
         },
-        bash_command='echo $START_DATE && echo $END_DATE' # A && B : A가 성공하면 B도 실행시킨다.
+        bash_command='echo $START_DATE && echo $END_DATE'
     )
 
     bash_t1 >> bash_t2
