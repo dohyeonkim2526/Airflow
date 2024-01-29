c84dc608bf3e973b87f26b04da460ce5322fecb9
dohyeonkim2526 <veronica.kdh@gmail.com>
2024-01-29 17:03:00 +0900

    openapi test upload

--- dags/dags_openapi_test2.py
+++ dags/dags_openapi_test2.py
@@ -11,39 +11,24 @@ with DAG(
     tags=['project']
 ) as dag:
     
-    # getRTMS_openapi_data = SimpleHttpOperator(
-    #     task_id='getRTMS_openapi_data',
-    #     http_conn_id='openapi.molit.go.kr',
-    #     endpoint='OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTrade',
-    #     method='GET',
-    #     headers={'Content-Type':'application/xml'},
-    #     data={'LAWD_CD':'11110',
-    #           'DEAL_YMD':'201512',
-    #           'serviceKey':'{{ var.value.apikey_getRTMS_openapi_molit }}'
-    #           },
-    #     log_response=True,
-    #     dag=dag
-    # )
-
-    # @task(task_id='call_python')
-    # def call_python(**kwargs):
-    #     ti=kwargs['ti']
-    #     result=ti.xcom_pull(task_ids='getRTMS_openapi_data')
-    #     print(result) 
-    
-    # getRTMS_openapi_data >> call_python()
-
-    ''' 서울시 공공자전거 대여소 정보 '''
-    tb_cycle_station_info=SimpleHttpOperator(
-        task_id='tb_cycle_station_info',
-        http_conn_id='openapi.seoul.go.kr', # airflow connection 정보
-        endpoint='{{var.value.apikey_openapi_seoul_go_kr}}/json/tbCycleStationInfo/1/10/',  # key는 변수로 사용
+    getRTMS_openapi_data = SimpleHttpOperator(
+        task_id='getRTMS_openapi_data',
+        http_conn_id='openapi.molit.go.kr',
+        endpoint='OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTrade',
         method='GET',
-        headers={'Content-Type': 'application/json',
-                 'charset': 'utf-8',
-                 'Accept': '*/*'
-                }
+        # headers={'Content-Type':'application/xml'},
+        headers={'LAWD_CD':'11110',
+              'DEAL_YMD':'201512',
+              'serviceKey':'{{ var.value.apikey_getRTMS_openapi_molit }}'
+              },
+        log_response=True,
+        dag=dag
     )
 
-    tb_cycle_station_info
+    @task(task_id='call_python')
+    def call_python(**kwargs):
+        ti=kwargs['ti']
+        result=ti.xcom_pull(task_ids='getRTMS_openapi_data')
+        print(result) 
     
+    getRTMS_openapi_data >> call_python()
